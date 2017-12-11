3109159bcd24a265a99a86ac5ab67b5f2f62f872
monarizasan <32188449+monarizasan@users.noreply.github.com>
2017-12-11 13:38:03 +0900

    Update fav_spam_blocker.py

--- fav_spam_blocker.py
+++ fav_spam_blocker.py
@@ -28,7 +28,7 @@ def spam_block(consumer_key, consumer_secret, token, token_secret, user_name, nu
     
         try:
             user = api.get_user(screen_name=target_screen_name)
-            api.destroy_block(target_screen_name)
+            api.create_block(target_screen_name)
         
         except:
             print(u"%sは存在しないか何らかの理由でブロックできませんでした。" % (target_screen_name))
@@ -50,7 +50,7 @@ def unlock(consumer_key, consumer_secret, token, token_secret, user_name, num_di
     
         try:
             user = api.get_user(screen_name=target_screen_name)
-            api.create_block(target_screen_name)
+            api.destroy_block(target_screen_name)
         
         except:
             print(u"%sは存在しないか何らかの理由でブロック解除できませんでした。" % (target_screen_name))
